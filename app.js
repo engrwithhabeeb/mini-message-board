b88f4d5970df27e4f3ab2045adb5f79cdddbf0b6
engrwithhabeeb <174087635+engrwithhabeeb@users.noreply.github.com>
2026-06-18 17:15:52 +0100

    chore(server): coordinate the app - manages path joining - registers routing to server - handles starting server

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
 const express = require("express");
 const path = require("node:path");
+const indexRouter = require("./router/indexRouter");
+const formRouter = require("./router/formRouter");
 
 const app = express();
 
-const PORT = undefined; // rename to your prefer port.
+const PORT = 3000;
 
-app.set("views", path.join(__dirname, "views")); // leave as is unless your ui directory is not views
-app.set("view engine", "ejs"); // don't change unless you are using another view engine apart from ejs
+const assetsPath = path.join(__dirname, "public");
 
-const ENGINEER = {
-  name: "Habeeb Abdullahi",
-  username: "engrwithhabeeb", // on socials worldwide.
-  linkedin: "https://linkedin.com/in/engrwithhabeeb"
-};
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index", {
-    ENGINEER: ENGINEER,
-  }); /* rendering view at index.ejs and passing data to it. 
-  Feel to change it to what you application needs */
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(assetsPath));
+app.use("/", indexRouter);
+app.use("/new", formRouter);
 
-app.listen(PORT || 8080 /* free to remove || 8080 after renaming PORT at the top or leave them as fallback */, () => {
-  console.log(
-    `App starting at ${PORT ? PORT : 8080 /* free to remove the condition ? PORT : 8080 after renaming PORT at the top or leave them as fallback */}`,
-  );
+app.listen(PORT || 8080, () => {
+  console.log(`App starting at ${PORT ? PORT : 8080}`);
 });
